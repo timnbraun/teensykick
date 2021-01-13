b05314de8387bdf76aebdb197a873e6a676cdecd
Tim Braun <tim@tim-braun.com>
2021-01-13 17:05:28 -0600

    Add periodic debug output on interrupt count.

--- hello_sine.cpp
+++ hello_sine.cpp
@@ -31,28 +31,33 @@
 #include <Audio.h>
 #include <usb_dev.h>
 
+#define dbg(...) \
+	fiprintf(stderr, __VA_ARGS__)
 
-AudioSynthWaveformSine   sine1;
-AudioOutputI2S        	 out;
-AudioConnection          patchCord1(sine1, 0, out, 0);
-AudioConnection          patchCord2(sine1, 0, out, 1);
 
-usb_serial_class Serial;
-elapsedMillis since_LED_switch;
+AudioSynthWaveformSine  sine1;
+AudioOutputI2S        	out;
+AudioConnection         patchCord1(sine1, 0, out, 0);
+AudioConnection         patchCord2(sine1, 0, out, 1);
 
-uint32_t Freq = 440;
+usb_serial_class 		Serial;
+elapsedMillis 			since_LED_switch, since_hello;
+elapsedMillis			interrupt_time;
+
+uint32_t	Freq = 440;
+bool 		ledState = true;
+uint32_t	counter, bigtime;
+uint32_t	interrupts_last, interrupts_delta;
 
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
 	usb_init();
-	AudioMemory(12);
+	AudioMemory(2);
 	sine1.frequency(Freq);
 	sine1.amplitude(1.0);
-	Serial.write("Hello sine\r\n");
+	dbg("Hello sine\r\n");
 }
-
-bool ledState = true;
 
 void loop()
 {
@@ -61,4 +66,28 @@ void loop()
 		digitalWriteFast(LED_BUILTIN, ledState? HIGH : LOW);
 		since_LED_switch = 0;
 	}
+	if (since_hello >= 1000) {
+		dbg(".");
+		if (++counter >= 10) {
+			uint32_t this_count = out.isrCount();
+			interrupts_delta = this_count - interrupts_last;
+			interrupts_last = this_count;
+			dbg(" %5lu %5lu %03lu\r\n", interrupts_delta,
+				(uint32_t )interrupt_time, bigtime++);
+			interrupt_time = 0;
+			counter = 0;
+		}
+		since_hello = 0;
+	}
+	while (Serial.available()) {
+		int incoming = Serial.read();
+		switch (incoming) {
+		case 'b':
+			dbg("sine used %u cycles\r\n", sine1.cpu_cycles_total );
+		break;
+		default:
+			Serial.write((uint8_t)incoming);
+		}
+	}
+	sine1.update();
 }
