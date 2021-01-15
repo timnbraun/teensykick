24ad52d58660dca30bb01cf954d37d0b2348d1e6
Tim Braun <tim@tim-braun.com>
2021-01-15 16:19:08 -0600

    Use synth_simple_drum for a kick sound, add midi triggering.

--- kick.cpp
+++ kick.cpp
@@ -31,45 +31,111 @@
 #include <Audio.h>
 #include <usb_dev.h>
 
+#define dbg_putc(c) \
+	fputc((c), stderr)
+
+void onNoteOn(byte chan, byte note, byte vel);
+void onNoteOff(byte chan, byte note, byte vel);
+
 AudioControlSGTL5000	dac;
-AudioSynthWaveformSine	sine1;
+AudioSynthSimpleDrum	kick;
 AudioOutputI2S        	out;
-AudioConnection         patchCord1(sine1, 0, out, 0);
-AudioConnection       	patchCord2(sine1, 0, out, 1);
+AudioConnection         patchCord1(kick, 0, out, 0);
+AudioConnection       	patchCord2(kick, 0, out, 1);
+
 usb_serial_class 		Serial;
-
 elapsedMillis 			since_LED_switch, since_hello;
+elapsedMillis			interrupt_time;
 
-uint32_t 				freq = 440;
-bool 					ledState = true;
-uint32_t				counter, bigtime;
+uint32_t		counter, bigtime;
+uint32_t		interrupts_last, interrupts_delta;
 
 
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
 	usb_init();
+
+	usbMIDI.setHandleNoteOn(onNoteOn);
+	usbMIDI.setHandleNoteOff(onNoteOff);
+
 	delay(1000);
 	AudioMemory(2);
+	kick.frequency(80);
+	kick.length(200);
+	kick.secondMix(0.25);
+	kick.pitchMod(0x2f0); // 0x200 is no mod...
+	dbg("\r\nHello teensy kick\r\n\r\n");
 	dac.enable();
-	sine1.frequency(freq);
-	sine1.amplitude(1.0);
-	dbg("Hello teensy kick\r\nAudio block size is %u\r\n", AUDIO_BLOCK_SAMPLES);
+	dac.lineOutLevel( 14 );
 }
 
 void loop()
 {
+	static bool run = true, levelHigh = true;
+
 	if (since_LED_switch > 500) {
-		ledState = !ledState;
-		digitalWriteFast(LED_BUILTIN, ledState? HIGH : LOW);
+		digitalToggleFast(LED_BUILTIN);
 		since_LED_switch = 0;
 	}
 	if (since_hello >= 1000) {
 		dbg(".");
 		if (++counter >= 10) {
-			dbg(" isr=%4lu %3lu\r\n", out.isrCount(), bigtime++);
+			uint32_t this_count = out.isrCount();
+			interrupts_delta = this_count - interrupts_last;
+			interrupts_last = this_count;
+			dbg(" isr=%4lu %5lu %3lu\r\n", interrupts_delta,
+				(uint32_t)interrupt_time, bigtime++);
+			interrupt_time = 0;
 			counter = 0;
 		}
 		since_hello = 0;
 	}
+	while (Serial.available()) {
+		int incoming = Serial.read();
+		switch (incoming) {
+		case 'b':
+			dbg("drum used %u cycles\r\n", kick.cpu_cycles_total );
+		break;
+		case ' ':
+			run = !run;
+			dbg("now %s\r\n", run? "running" : "stopped");
+			if (run) {
+				kick.noteOn();
+			}
+			else {
+				kick.noteOn(0x6000);
+			}
+		break;
+		case 'l':
+			if (levelHigh) {
+				dac.lineOutLevel( 28 );
+			}
+			else {
+				dac.lineOutLevel( 14 );
+			}
+			levelHigh = !levelHigh;
+			dbg("level now %s\r\n", levelHigh? "high" : "low");
+		break;
+		case 'r':
+			_reboot_Teensyduino_();
+		break;
+		default:
+			dbg_putc(incoming);
+		}
+	}
+
+	usbMIDI.read();
+}
+
+void onNoteOn(byte chan, byte note, byte vel)
+{
+	uint32_t v = vel << 8;
+	dbg("N %d on %lu\r\n", note, v);
+	kick.noteOn(v);
+}
+
+void onNoteOff(byte chan, byte note, byte vel)
+{
+	dbg("N %d off\r\n", note);
 }
