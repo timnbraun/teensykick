35eaeb5439dfde2bec24859a43a928efe214a1c8
Tim Braun <tim@tim-braun.com>
2021-01-27 21:03:34 -0600

    Prepend CR if written data ends in LF

--- usb_write.cpp
+++ usb_write.cpp
@@ -10,10 +10,15 @@
 
 #include <usb_serial.h>
 
-extern "C" { 
+extern "C" {
 
 __attribute__((weak)) int _write(int file, char *ptr, int len)
 {
+	if (ptr[len-1] == '\n') {
+		usb_serial_write((const void *)ptr, len-1);
+		usb_serial_write((const void *)"\r\n", 2);
+		return len;
+	}
 	usb_serial_write((const void *)ptr, len);
 	return len;
 }
